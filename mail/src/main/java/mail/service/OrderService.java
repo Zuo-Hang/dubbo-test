98f3ee464756389c4b05224c1530a566f89ae0c9
zuo_h <zuo_hang97@163.com>
2021-05-16 02:15:36 +0800

    base

---
+++ mail/src/main/java/mail/service/OrderService.java
@@ -0,0 +1,16 @@
+package mail.service;
+
+
+import mail.bean.UserAddress;
+
+import java.util.List;
+
+public interface OrderService {
+	
+	/**
+	 * 初始化订单
+	 * @param userId
+	 */
+	public List<UserAddress> initOrder(String userId);
+
+}
